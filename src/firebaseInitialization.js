dcd238267eb3e21bdd72d8a8fe9954bb370809d0
jedierikb <erikb@media.mit.edu>
2020-03-26 18:46:08 -0400

    credentials

--- src/firebaseInitialization.js
+++ src/firebaseInitialization.js
@@ -6,12 +6,12 @@
 
 //get this from firebase console
 var firebaseConfig = {
-  apiKey: "...",
-  authDomain: "...",
-  databaseURL: "...",
-  projectId: "...",
-  storageBucket: "...",
-  messagingSenderId: "...",
-  appId: "..."
+  apiKey: "AIzaSyCCo7Hqy6LOnJ1c2z5Jnw-wVwn8l7MvHac",
+  authDomain: "fir-example-4a5b8.firebaseapp.com",
+  databaseURL: "https://fir-example-4a5b8.firebaseio.com",
+  projectId: "fir-example-4a5b8",
+  storageBucket: "fir-example-4a5b8.appspot.com",
+  messagingSenderId: "363005169446",
+  appId: "1:363005169446:web:fe96a910ef9736eade7be7"
 };
 firebase.initializeApp( firebaseConfig );
